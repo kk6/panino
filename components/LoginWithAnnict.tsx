e8ee4ac4287d6e012ab878975b8a5b19ac295d2e
kk6 <hiro.ashiya@gmail.com>
2021-09-12 06:16:58 +0900

    :lipstick: Chakura UI 導入

---
+++ components/LoginWithAnnict.tsx
@@ -0,0 +1,16 @@
+import { Button, Flex, Heading, Stack } from "@chakra-ui/react";
+
+export const LoginWithAnnict = (): JSX.Element => {
+  return (
+    <Flex minH={"100vh"} align={"center"} justify={"center"}>
+      <Stack spacing={4} w={"full"} maxW={"md"} p={6} my={12}>
+        <Heading lineHeight={1.1} fontSize={{ base: "2xl", md: "3xl" }}>
+          Panino
+        </Heading>
+        <Stack spacing={6}>
+          <Button colorScheme="pink">Annict でログイン</Button>
+        </Stack>
+      </Stack>
+    </Flex>
+  );
+};
