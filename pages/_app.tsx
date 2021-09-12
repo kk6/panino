b8791da3568d4900971f7372479cf3ca91d7dcb0
kk6 <hiro.ashiya@gmail.com>
2021-09-13 00:01:04 +0900

    :adhesive_bandage: fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from "next/app"
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </ChakraProvider>
   )
 }
