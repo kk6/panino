926ad601fbcb85ea23440c5f0ac5c588b1904762
kk6 <hiro.ashiya@gmail.com>
2021-09-25 19:44:02 +0900

    :rotating_light: Fix #2

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -2,5 +2,14 @@ import { ApolloClient, InMemoryCache } from "@apollo/client"
 
 export const client = new ApolloClient({
   uri: "https://api.annict.com/graphql",
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      User: {
+        merge(existing, incoming) {
+          if (!existing) return incoming
+          return { ...existing, ...incoming }
+        },
+      },
+    },
+  }),
 })
